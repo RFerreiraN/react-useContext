5457a28a8dc23f60cd344c8d7891b7fc1e8e9f6d
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-09-18 22:40:04 +0200

    creamos barra de navegacion del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { NavBar } from './Components/NavBar'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { Home } from './Routes/Home'
+import { Productos } from './Routes/Productos'
+import { Carrito } from './Routes/Carrito'
+import { Login } from './Routes/Login'
+
+export const App = () => {
+  return (
+    <>
+      <NavBar/>
+        <Routes>
+          <Route path='/' element={ <Home/>}></Route>
+          <Route path='/productos' element={ <Productos/> }></Route>
+          <Route path='/carrito' element={ <Carrito/> }></Route>
+          <Route path='/login' element={ <Login/>}></Route>
+          <Route path='/*' element={  <Navigate to='/'/> }></Route>
+        </Routes>
+    </>
+  )
+}
